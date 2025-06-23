e52e892de9c3b8bb6ca0a0b7ab1f21a7acd4c504
Wolvye <alainsoltau@googlemail.com>
2025-06-23 10:54:28 +0200

    Update

--- MAUIVERTER/MVVM/Views/MenuView.xaml.cs
+++ MAUIVERTER/MVVM/Views/MenuView.xaml.cs
@@ -1,3 +1,5 @@
+using MAUIVERTER.MVVM.ViewModels;
+
 namespace MAUIVERTER.MVVM.Views;
 
 public partial class MenuView : ContentPage
@@ -5,5 +7,6 @@ public partial class MenuView : ContentPage
 	public MenuView()
 	{
 		InitializeComponent();
+		BindingContext = new MenuViewModel();
 	}
 }
